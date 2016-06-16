fa4b072301e8ba9b887b2c0b9098e854fc8400a5
adriano barbosa <b.adrianobarbosa@gmail.com>
2016-06-16 00:00:55 -0300

    Criação da Base de Dados

--- src/Alfa/Dao/BaseDeDados.php
+++ src/Alfa/Dao/BaseDeDados.php
@@ -11,28 +11,47 @@ class BaseDeDados implements \Alfa\Abstracao\BaseDeDados {
 
     private $nome;
     private static $conexao;
+
+    /**
+     *
+     * @var \Alfa\Abstracao\SGBD 
+     */
     private $sgbd;
 
+    /**
+     * 
+     * @param \Alfa\Abstracao\SGBD $server
+     * @param string $nome
+     */
     function __construct(\Alfa\Abstracao\SGBD $server, $nome = null) {
         $this->sgbd = $server;
         $this->nome;
     }
 
+    /**
+     * 
+     * @return \PDO
+     * @throws \Exception
+     */
     public function conectar() {
         if (!self::$conexao) {
             if (!$this->nome) {
                 throw new \Exception("Não é possível conectar sem informar o nome da base de dados!");
             }
             try {
-                self::$conexao = new \PDO($dsn, $username, $passwd, $options);
+                self::$conexao = new PDO("mysql:host={$this->sgbd->getEndereco()};"
+                        . " port={$this->sgbd->getPorta()}; "
+                        . " dbname={$this->getNome()}", $this->sgbd->getUsuario()
+                        , $this->sgbd->getSenha(), array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             } catch (\Exception $exc) {
                 throw new \Exception("Erro ao tentar iniciar uma conexão! " . $exc->getMessage());
             }
         }
+        return self::$conexao;
     }
 
     public function desconectar() {
-        
+        self::$conexao = null;
     }
 
     function getNome() {
